7c6406c96b9b679ea3b03f7719fc99fd7ee16980
Muniz <odranoelo_O@hotmail.com>
2017-03-15 21:27:00 -0300

    - Finished login one

--- src/components/views/login/login-one.vue
+++ src/components/views/login/login-one.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="flex justify-center">
-    <div class="card  text-white text-center" id="login">
-      <div class="card-title bg-deep-purple-7">
-        <svg id="logo" viewBox="0 0 463 463" style="enable-background:new 0 0 463 463;" xml:space="preserve" width="128px" height="128px" v-html="logo" >
+  <div class="flex justify-center full-height" :class="heightSize">
+    <div class="row gt-sm">
+      <div class="row">
+        <div class="list">
+          <label class="item" v-for="color in colors">
+            <div class="item-primary">
+              <q-radio v-model="bgColor" :val="color"></q-radio>
+            </div>
+            <div class="item-content capitalize">
+              {{color}}
+            </div>
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="list fit">
+          <label class="item" v-for="tone in colorTones">
+            <div class="item-primary">
+              <q-radio v-model="toneColor" :val="tone"></q-radio>
+            </div>
+            <div class="item-content capitalize">
+              {{tone}}
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="card text-white text-center width-3of4 bg-width-2of5 gt-bg-width-1of4 sm-auto " id="login">
+      <div class="card-title" :class="finalBgColor">
+        <svg id="logo" viewBox="0 0 483 483"
+             style="enable-background:new 0 0 460 460;"
+             xml:space="preserve" width="128px" height="128px"
+             v-html="logoMethod">
         </svg>
-        <h4>Digitalizer</h4>
+        <h4>{{logo}}</h4>
       </div>
       <div class="card-content bg-white ">
         <div class="stacked-label">
@@ -16,9 +45,37 @@
           <label>Password</label>
         </div>
       </div>
-      <div class="card-actions inline-block">
-        <button class="teal raised outline fit" @click="login()">Login</button>
-        <button class="red raised outline fit" @click="login()">Login Google</button>
+      <div class="card-actions inline-block vertical-middle">
+        <button class="teal fit " :class="buttonClass" @click="login()">Login</button>
+        <button class="red fit" :class="buttonClass" @click="login()">Login Google</button>
+        <button class="blue fit" :class="buttonClass" @click="login()">Login Facebook</button>
+      </div>
+    </div>
+    <div class="row gt-sm">
+      <div class="row ">
+        <div class="list fit">
+          <label class="item" v-for="bClass in buttonClasses">
+            <div class="item-primary">
+              <q-radio v-model="buttonClass" :val="bClass"></q-radio>
+            </div>
+            <div class="item-content capitalize">
+              {{bClass}}
+            </div>
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="list">
+          <label class="item" v-for="logoName in logos">
+            <div class="item-primary">
+              <q-radio v-model="logo" :val="logoName"></q-radio>
+            </div>
+            <div class="item-content capitalize">
+              {{logoName}}
+            </div>
+          </label>
+          <button class="teal raised full-width " @click="startAnimation()">Animate !</button>
+        </div>
       </div>
     </div>
   </div>
@@ -28,25 +85,41 @@
   /* eslint-disable */
   import {mapMutations} from 'vuex'
   import Vivus from 'vivus'
-  import fingerPrint from './logoData'
+  import logoData from './logoData'
+  import { Platform } from 'quasar'
   export default {
     mounted () {
       this.setLayoutNeeded(false)
       this.setIsLoginPage(true)
-      new Vivus('logo', {
-          duration: 400
-        }, function(element) {
-          for (let item of element.el.children[0].children) {
-              item.setAttribute('style', 'fill:white')
-          }
+      this.startAnimation()
+    },
+    computed: {
+      heightSize (){
+        if (Platform.is.desktop) {
+          return 'items-center'
         }
-      )
+        return ''
+      },
+      finalBgColor () {
+        return `bg-${this.bgColor}-${this.toneColor}`
+      },
+      logoMethod () {
+        return logoData[this.logo]
+      }
     },
     data () {
       return {
-        logo: fingerPrint,
+        logos: Object.keys(logoData),
+        logo: 'Digitalizer',
         email: 'quasar@admin.com',
-        password: '123456'
+        password: '123456',
+        bgColor: 'purple',
+        toneColor: 10,
+        colors: ['purple', 'blue', 'red', 'green', 'amber'],
+        colorTones: ['2', '4', '6', '8', '10'],
+        buttonClasses: ['normal','outline', 'clear', 'push', 'bordered', 'round'],
+        buttonClass: 'normal',
+        vivus: ''
       }
     },
     methods: {
@@ -55,15 +128,28 @@
         this.setLayoutNeeded(true)
         this.setIsLoginPage(false)
         this.$router.push('/')
+      },
+      startAnimation () {
+        this.vivus = new Vivus('logo', {
+            duration: 400,
+          forceRender: false
+          }, function(element) {
+            for (let item of element.el.children[0].children) {
+              item.setAttribute('style', 'fill:white')
+              item.setAttribute('style', 'fill:white')
+            }
+          }
+        )
       }
     }
   }
 </script>
-<style >
-  #login {
-    margin-top: 5%;
-    width: 300px;
-    height: 500px;
+<style scoped>
+  .card {
+    margin-bottom: 0px;
+  }
+  .card-content {
+    min-height: 160px;
   }
   button {
     margin-bottom: 4%;
