32c777ed74e01dff66f77177fc161ce7a05e869a
muniz <leonzephiro@gmail.com>
2019-06-05 19:40:56 -0300

    updating pricing

---
+++ src/components/ACardDoc.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <q-card >
+
+    <q-toolbar :class="toolbarClass" >
+      <section class="text-subtitle1">
+        <div class="doc-card-title">
+          {{title}}
+        </div>
+      </section>
+      
+      <q-toolbar-title />
+
+    </q-toolbar>
+
+    <q-separator />
+
+    <q-card-section class="bg-white">
+      <slot name="body-one" />
+    </q-card-section >
+
+    <q-separator />
+
+    <q-card-section class="bg-grey-2" >
+      <slot name="body-two" />
+    </q-card-section>
+
+  </q-card>
+</template>
+
+<script>
+export default {
+  props:  {
+    toolbarClass: String,
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      test: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+.doc-card-title {
+  margin-left: -24px;
+  padding: 2px 10px 2px 24px;
+  background: #e0e0e0;
+  color: #616161;
+  position: relative;
+  border-radius: 3px 5px 5px 0;
+}
+
+.doc-card-title:after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 0;
+  height: 0;
+  border: 0 solid transparent;
+  border-top-color: #bebebe;
+  border-width: 10px 0 0 12px;
+}
+</style> 
---
+++ src/components/pricing/CardPlanOne.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-card>
+    <q-card-title :class="titleClasses">
+      <span class="text-white">
+        <h5>{{ title }}</h5>
+      </span>
+    </q-card-title>
+    <q-card-main>
+      <h3>${{ price }}</h3>
+      <h6 class="text-italic">{{ priceSubtitle }}</h6>
+    </q-card-main>
+    <q-card-actions> 
+      <slot name="body"></slot>
+    </q-card-actions>
+    <div class="card-content bg-white">
+      <q-btn :color="buttonClasses" outline class="fit" @click="planSelected()"
+        >Choose</q-btn
+      >
+    </div>
+  </q-card>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: [
+    "title",
+    "titleClasses",
+    "price",
+    "priceSubtitle",
+    "buttonClasses",
+    "cardId"
+  ],
+  data() {
+    return {};
+  },
+  methods: {
+    planSelected() {
+      this.$emit("card-selected", this.cardId);
+    }
+  }
+};
+</script>
+<style scoped>
+.label {
+  min-height: 50px;
+}
+h5 {
+  font-weight: 300;
+  flex: 1 1 auto;
+}
+h3,
+h6 {
+  font-weight: 300;
+}
+.label.pointing-up:before,
+.label.pointing-right:before,
+.label.pointing-down:before,
+.label.pointing-left:before {
+  width: 1.4rem;
+  height: 1.4rem;
+}
+.q-card {
+  width: 332px;
+}
+</style>
